fb80508975e09a4cd88c350511a572f90948a7f9
Laury Kibamba <lorychris@icloud.com>
2026-04-30 11:06:26 +0200

    Add: n-queens - submission-2

---
+++ Data Structures & Algorithms/n-queens/submission-2.py
@@ -0,0 +1,35 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+
+        column = set()
+        positiv = set()
+        negativ = set()
+
+        res = []
+
+        board = [["."] * n for i in range(n)]
+
+        def backtrack(r):
+            if r == n:
+                copy = ["". join(row) for row in board]
+                res.append(copy)
+                return
+            
+            for c in range(n):
+                if c in column or (r+c) in positiv or (r-c) in negativ:
+                    continue
+                
+                column.add(c)
+                positiv.add(r+c)
+                negativ.add(r-c)
+                board[r][c] = "Q"
+
+                backtrack(r+1)
+
+                column.remove(c)
+                positiv.remove(r+c)
+                negativ.remove(r-c)
+                board[r][c] = "."
+
+        backtrack(0)
+        return res
