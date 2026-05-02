299f95051d3233317addc2ba909041ae8903297c
Laury Kibamba <lorychris@icloud.com>
2026-05-02 13:54:08 +0200

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,13 @@
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groupes = collections.defaultdict(list)
+
+        for mot in strs:
+            compteur = [0] * 26
+            for lettre in mot:
+                index = ord(lettre) - ord("a")
+                compteur[index] +=1
+            cle_signature = tuple(compteur)
+            groupes[cle_signature].append(mot)
+        return list(groupes.values())
